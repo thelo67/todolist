e4c0e9b4366ca028346d0b167158db803a770929
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-02-03 18:19:09 +0200

    Update App component for AddItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AddItem from "./AddItem";
 import ToDoList, { Item } from "./ToDoList";
 
 const initialList = [
@@ -16,6 +17,12 @@ const initialList = [
   },
 ];
 
+
+const isPartOf = (item: Item, items: Item[]): boolean => {
+  return items.some((it) => it.priority === item.priority);
+};
+
+
 class App extends React.Component<{}, { items: Item[] }> {
 
   constructor(props: any) {
@@ -23,12 +30,26 @@ class App extends React.Component<{}, { items: Item[] }> {
     this.state = {
       items: initialList,
     };
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(item: Item) {
+    const { items } = this.state;
+  
+    if (isPartOf(item, items)) {
+      alert(`Item with priorirty: ${item.priority} exists`);
+      return;
+    }
+    this.setState({
+      items: items.concat(item),
+    });
   }
 
   render() {
     const { items } = this.state;
     return (
       <div className="App">
+        <AddItem addItem={this.addItem} />
         <br />
         <ToDoList items={items} />
       </div>
